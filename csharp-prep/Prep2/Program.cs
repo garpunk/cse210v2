7f0d77bbb69905e2b2f64e6d44a361ce4adbbc99
garpunk <garpunked@gmail.com>
2025-01-11 17:20:43 -0700

    completed prep 1 and prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Input your score to see your grade");
+        string valueFromUser = Console.ReadLine();
+
+        int grade = int.Parse(valueFromUser);
+
+        string letter = "F";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"{letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed!");
+        }
     }
 }
