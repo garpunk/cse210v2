3e3818c80f5566c5d32ebe0195fd7dbe8f1b79ef
garpunk <garpunked@gmail.com>
2025-01-22 16:37:27 -0700

    did prep week3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Browning";
+        job1._jobTitle = "Machinist";
+        job1._startYear = 2021;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "BYUI";
+        job2._jobTitle = "Podcast Host";
+        job2._startYear = 2024;
+        job2._endYear = 2027;
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Garrett Helms";
+
+        resume1._jobList.Add(job1);
+        resume1._jobList.Add(job2);
+
+        resume1.Display();
+
     }
+   
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
+using System.IO.Pipes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+       Movie favoriteMovie = new Movie();
+       favoriteMovie._title = "Star Wars";
+       favoriteMovie._year = 1977;
+       favoriteMovie._runtime = 150;
+       favoriteMovie._rating = "PG";
+
+       Movie otherMovie = new Movie();
+       otherMovie._title = "Avatar";
+       otherMovie._year = 2009;
+       otherMovie._rating = "PG-13";
+       otherMovie._runtime = 162;
+
+       favoriteMovie.Display();
+
+       DisplayMovie(favoriteMovie);
+       DisplayMovie(otherMovie);
+
+    }
+
+    static void DisplayMovie(Movie aMovie)
+    {
+     Console.WriteLine($"{aMovie._title} - {aMovie._year}");
     }
 }
+
